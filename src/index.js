1d5cc29541230e154d8b28c2f22186a0215a92bd
bgocean <bigocean700@gmail.com>
2024-08-30 11:14:56 +0300

    add router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {Home} from './pages/Home'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Home } from "./pages/Home";
+import { Product } from "./pages/Product"; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/product",
+    element: <Product />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-
